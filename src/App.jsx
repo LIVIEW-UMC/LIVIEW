517fa1854213c4ddefd01f9757edfd120abeca48
myeanji <hyean03@naver.com>
2024-01-12 12:39:06 +0900

    feat: create setting sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
+import SettingsSidebar from './components/SettingsSidebar';
 
 const App = () => (
   <div className="App">
     <Nav />
+    <SettingsSidebar />
     <Footer />
   </div>
 );
