eb04a8cf96f3da8cc298bebfff4240d15a8ce61f
한지은 <hjn510450@gmail.com>
2024-01-17 16:17:43 +0900

    refactor: Masonry layout to use 'egjs/react-infinitegrid'

--- src/components/main/Gallery.jsx
+++ src/components/main/Gallery.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { MasonryInfiniteGrid } from '@egjs/react-infinitegrid';
 
 const imageContext = require.context('../../assets/dummy', false, /\.(jpg)$/);
 
@@ -6,18 +7,13 @@ function Gallery() {
   const images = imageContext.keys().map(imageContext);
 
   return (
-    <GalleryContainer>
+    <MasonryInfiniteGrid column={5} gap={21} defaultDirection={'end'} align={'justify'} threshold={1000} style={{ width: '1007px' }}>
       {images.map((image, index) => (
         <GalleryItem key={index + 1} src={image} alt={`img-${index}`} />
       ))}
-    </GalleryContainer>
+    </MasonryInfiniteGrid>
   );
 }
-
-const GalleryContainer = styled.div`
-  width: 1007px;
-  column-count: 5;
-`;
 
 const GalleryItem = styled.img`
   width: 191px;
