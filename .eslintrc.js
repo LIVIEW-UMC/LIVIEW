ab26a91727fe0105e2ab9be1b2498ec1494ab122
한지은 <hjn510450@gmail.com>
2024-01-17 16:12:43 +0900

    chore: update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,6 +26,10 @@ module.exports = {
     'react/no-array-index-key': 'off',
     'react/prop-types': 'off',
     'operator-linebreak': 'off',
-    'linebreak-style': ['error', 'unix'],
+    'linebreak-style': 'off',
+    'react/jsx-curly-brace-presence': 'off',
+    'no-nested-ternary': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+    'max-len': 'off'
   },
 };
