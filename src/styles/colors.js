ed00caa8ea69084caaeaca44eac466ed44b3318d
한지은 <hjn510450@gmail.com>
2024-01-18 20:38:22 +0900

    feat: add color variables

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -4,6 +4,7 @@ const colors = {
   gray: '#697077',
   lightGray: '#E1E1E1',
   ivoryGray: '#F5F5F5',
+  darkGray: '#909090',
   DarkBrownGray: '#464646',
   profilNnameColor: '#393939',
   profileEmailColor: '#A4A4A4',
