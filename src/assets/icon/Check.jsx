8e6887194022a580d4658e6bc183a1440213017c
한지은 <hjn510450@gmail.com>
2024-02-04 19:15:27 +0900

    feat: modify check icon

--- src/assets/icon/Check.jsx
+++ src/assets/icon/Check.jsx
@@ -1,7 +1,7 @@
-const Check = () => (
-  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+const Check = ({ stroke }) => (
+  <svg width="14" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
     <g id="check">
-      <path id="Vector" d="M16.6668 5L7.50016 14.1667L3.3335 10" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+      <path id="Vector" d="M16.6668 5L7.50016 14.1667L3.3335 10" stroke={stroke} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
     </g>
   </svg>
 );
