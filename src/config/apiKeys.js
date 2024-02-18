ef354dc776e89131da821639b3741a3c0b4d8f86
kimminseo <124893976+minnnnseo@users.noreply.github.com>
2024-02-18 15:44:17 +0900

    feat: Update settings.

---
+++ src/config/apiKeys.js
@@ -0,0 +1,5 @@
+export const NAVER_CLIENT_ID = 'lTccX_qPUQRvOPSbq9mY';
+export const NAVER_CLIENT_SECRET = 'gzDmMtx2EY';
+export const NAVER_MAP_CLIENT_ID = '391ndl9tbu';
+export const NAVER_MAP_CLIENT_SECRET = 'Dk1lCySSrAbimstCp4bTYwgeuBaNguv7tq6JkhOg';
+export const TMAP_APP_KEY = '8uydAMpM5M1fH25P7Y6e88uF941OXKv2KG1kizC8';
